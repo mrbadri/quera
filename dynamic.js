e0dfbdf5fad8b0bedf8f8e9621b4e83eecd874ec
MrBadri <badri0000badri@gmail.com>
2022-05-20 01:09:29 +0430

    CHORE: handle moveing crosshair base on moveing mouse

--- dynamic.js
+++ dynamic.js
@@ -1,1 +1,9 @@
 /* write your code here ... */
+// variables
+const $crosshair = document.querySelector(".crosshair");
+
+// Events
+document.addEventListener("mousemove", (e) => {
+  $crosshair.style.setProperty("--x", `${e.pageX}px`);
+  $crosshair.style.setProperty("--y", `${e.pageY}px`);
+});

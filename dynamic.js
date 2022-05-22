a714086449802655b5b9882b3d62c2534c9e6a98
mohammad.b <mohammad.b@arianalabs.io>
2022-05-22 18:42:07 +0430

    FEAT: handle move bullet

--- dynamic.js
+++ dynamic.js
@@ -1,9 +1,10 @@
 /* write your code here ... */
 // variables
 const $body = document.querySelector("body");
-const bodywidth = $body.offsetWidth;
 const $crosshair = document.querySelector(".crosshair");
-const $pistol = document.querySelector(".pistol img");
+const $Pistol = document.querySelector(".pistol");
+
+$body.style.overflow = "hidden";
 
 // Events
 // --- on mouse move
@@ -13,27 +14,51 @@ document.addEventListener("mousemove", (e) => {
   $crosshair.style.setProperty("--y", `${e.pageY}px`);
 
   // Handle pistol
-  const { x, y } = $pistol.getBoundingClientRect();
+  const { x, y } = $Pistol.getBoundingClientRect();
+  const rotate = handleRotate({ x: e.pageX - x, y: e.pageY - y });
 
-  const rotate = HandleRotate({ x: e.pageX - x, y: e.pageY - y });
-  $pistol.style.transform = `rotate(${rotate}deg)`;
+  $Pistol.style.transform = `rotate(${rotate}deg)`;
 });
 
 // --- on click
 document.addEventListener("click", (e) => {
   if (fireAccess(e.target)) {
-    console.log("run if");
+    const $bullet = createBullet();
+    let positionX = 0;
+
+    $Pistol.appendChild($bullet);
+
+    setInterval(function () {
+      positionX++;
+      $bullet.style.transform = `translate(${positionX}px `;
+    }, 4);
+
+    setTimeout(() => {
+      $bullet.remove();
+    }, 2000);
   }
 });
 
 // functions
-function HandleRotate({ x, y }) {
+function handleRotate({ x, y }) {
   let rotateRad = Math.atan2(y, x);
   let rotateDeg = (rotateRad * 180) / Math.PI;
 
   return Number(rotateDeg);
 }
 
+function handleSlopeMove({ input, x, y }) {
+  return (y / x) * input;
+}
+
 function fireAccess($target) {
   return !$target.classList.contains("death-zone");
+}
+
+function createBullet() {
+  const $bullet = document.createElement("img");
+  $bullet.classList.add("bullet");
+  $bullet.src = "/illustrations/bullet.svg";
+
+  return $bullet;
 }
